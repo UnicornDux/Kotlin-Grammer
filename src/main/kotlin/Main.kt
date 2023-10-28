cbd697f23f59d0ac9e57927a4f86b50a03805a87
huhaifeng <huhaifeng@wpdata.com>
2023-10-28 10:22:46 +0800

    repository init

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+import base.branch.branch
+import base.function.function
+import base.loop.loop
+import base.method.method
+import base.oop.data
+import base.oop.oop
+import base.oop.seal
+import base.oop.testEnum
+import base.variable.range
+import base.variable.variable
+
+
+fun main(args: Array<String>) {
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+
+}
